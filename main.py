f5fcff1723bc737dbc85055d36670d075076fda7
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-02-21 14:10:38 -0300

    Adição de 3 funções para capturar a opção do usuário já tratando os erros de operação

--- main.py
+++ main.py
@@ -6,16 +6,47 @@ def clear():
 def pause():
     input("Pressione ENTER para continuar. . .")
 
-def main():
+# Função para exibir o menu
+def menu():
+    print("=== CALCULADOR DE COMBUSTIVEL ===\n")
+    print("Escolha o Combustível:")
+    print("(1) GASOLINA")
+    print("(2) ETANOL")
+    print("(3) DISEL")
+    print("(0) SAIR")
+
+# Função para ler a opção do usuário
+def read_option():
+    return input("Sua opção: ")
+
+# Função para validar a opção do usuário
+def validar_option(option):
+    try:
+        option = int(option)
+        if option in [0, 1, 2, 3]:
+            return option
+        else:
+            return None
+    except ValueError:
+        return None
+
+# Função para obter a opção do usuário, garantindo que seja válida
+def get_option():
     while True:
         clear()
-        print("=== CALCULADOR DE COMBUSTIVEL ===\n")
-        print("Escolha o Combustível:")
-        print("(1) GASOLINA")
-        print("(2) ETANOL")
-        print("(3) DISEL")
-        print("(0) SAIR")
-        option = int(input("Sua opção: "))
+        menu()
+        user_input = read_option()
+        option = validar_option(user_input)
+
+        if option is not None:
+            return option
+        else:
+            print("\nOpção inválida! Tente novamente.")
+            pause()
+        
+def main():
+    while True:
+        option = get_option()
 
         if option == 1:
             print("Gasolina")
